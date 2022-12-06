334b4aa776802b5d4e0cd76e3eedcb9b964e3ea8
Jamalov <Jamalovn07@gmail.com>
2022-12-06 13:21:01 +0500

    commit for subcategory swagger

--- src/subCategories/subCategories.controller.ts
+++ src/subCategories/subCategories.controller.ts
@@ -7,29 +7,39 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { SubCategoriesDto } from './dto/create-subCategories.dto';
 import { UpdateSubCategoriesDto } from './dto/update-subCategories.dto';
+import { SubCategories } from './subCategories.model';
 import { SubCategoriesService } from './subCategories.service';
 
 @Controller('subcategories')
 export class SubCategoriesController {
   constructor(private readonly subcategoriesService: SubCategoriesService) {}
 
+  @ApiOperation({ summary: 'barcha subcategorylarni olish' })
+  @ApiResponse({ status: 200, type: [SubCategories] })
   @Get()
   getAll() {
     return this.subcategoriesService.getAll();
   }
 
+  @ApiOperation({ summary: 'bitta subcategoryni olish' })
+  @ApiResponse({ status: 200, type: SubCategories })
   @Get(':id')
   getOne(@Param('id') id: number) {
     return this.subcategoriesService.getOne(id);
   }
 
+  @ApiOperation({ summary: 'yangi subcategory qoshish' })
+  @ApiResponse({ status: 200, type: SubCategories })
   @Post()
   create(@Body() subCategoriesBody: SubCategoriesDto) {
     return this.subcategoriesService.create(subCategoriesBody);
   }
 
+  @ApiOperation({ summary: 'subcategoryni ozgartirish' })
+  @ApiResponse({ status: 200, type: SubCategories })
   @Put(':id')
   update(
     @Param('id') id: number,
@@ -38,6 +48,8 @@ export class SubCategoriesController {
     return this.subcategoriesService.update(id, subCategoriesBody);
   }
 
+  @ApiOperation({ summary: 'subcategoryni ochirish' })
+  @ApiResponse({ status: 200, type: SubCategories })
   @Delete(':id')
   delete(@Param('id') id: number) {
     return this.subcategoriesService.delete(id);
