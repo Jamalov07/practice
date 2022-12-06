1eed101d195ca06a1499d28a413077d0015d9071
Jamalov <Jamalovn07@gmail.com>
2022-12-06 12:56:22 +0500

    commit for creating required elements

--- src/categories/dto/create-categories.dto.ts
+++ src/categories/dto/create-categories.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString } from "class-validator";
 
 export class CategoriesDto {
     @ApiProperty({ example: "elektronics", description: "category nomi" })
-    @IsNotEmpty()
-    @IsString()
+    @IsNotEmpty({message:"category name bo'sh bo'lmasligi kerak"})
+    @IsString({message:"category name string bo'lishi kerak"})
     readonly category_name: string;
 }
